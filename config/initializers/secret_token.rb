cc3b466b1455f9bde802a2714bda73f981ced6e7
phipsi71 <phipsi@gmx.ch>
2013-06-10 21:20:34 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trspect::Application.config.secret_token = '297fcf36084ed0b9e0314b2944dbdb3c8d5f89f755c48f26841045c248fba0c0268d727ca415da4844cf000b4383961bbe2d390ad4389d067f3d7ad3ca3faf7c'
